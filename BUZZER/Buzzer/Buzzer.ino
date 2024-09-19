11053423593952b7049828d3ee804638645b0d39
ProfETEEverton <contato@evertonfigueiredo.com.br>
2024-09-19 19:17:16 -0300

    Código do 'Compasso' atualizado

--- BUZZER/Buzzer/Buzzer.ino
+++ BUZZER/Buzzer/Buzzer.ino
@@ -1,8 +1,10 @@
 int buzzer = 10;
+int button = 9;
+int ciclo = 0;
+int maximo = 2;
+bool tocando = false;
 
-void setup() {
-  pinMode(buzzer, OUTPUT);
-}
+int estado = 0;
 
 void dois(){
   tone(buzzer, 700, 500);
@@ -49,6 +51,44 @@ void quatro(){
   delay(500);
 }
 
-void loop() {
+void trocaC(){
+  if (ciclo == 0){
+    dois();
+  }else if(ciclo == 1){
+    tres();
+  }else if(ciclo == 2){
+    quatro();
+  }
+}
+
+void lerB(){
+  int leitura = digitalRead(button);
+  if (leitura == HIGH && estado == 0){
+    estado = 1;
+    tocando = true;
+    ciclo++;
+    if (ciclo > maximo){
+      ciclo = 0;
+    }
+  }else if (leitura == LOW){
+    estado = 0;
+
+  } 
+
+}
+
+
+void setup(){
+  pinMode(buzzer, OUTPUT);
+  pinMode(button, INPUT);
+}
+
+
+
+void loop(){
+  lerB();
   
+  if (tocando){
+    trocaC();
+  }
 }
